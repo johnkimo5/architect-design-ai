7692c7082831bda2781a79a50ab9824f139ce16a
John Kim <kimjohndev@gmail.com>
2026-01-04 01:28:02 -0500

    last fix

--- my-app/src/app/board/[id]/page.tsx
+++ my-app/src/app/board/[id]/page.tsx
@@ -1,6 +1,15 @@
 import { getBoard } from '@/app/actions'
-import BoardEditor from './BoardEditor'
+import dynamic from 'next/dynamic'
 import { notFound } from 'next/navigation'
+
+const BoardEditor = dynamic(() => import('./BoardEditor'), {
+  ssr: false,
+  loading: () => (
+    <div className="h-screen flex items-center justify-center">
+      <div className="text-gray-500">Loading editor...</div>
+    </div>
+  ),
+})
 
 interface PageProps {
   params: Promise<{ id: string }>
